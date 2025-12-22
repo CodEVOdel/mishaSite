284bd082785825454ee19c2164f38c3463835a9d
SadDog <avakyan.nikolay@gmail.com>
2025-12-22 19:10:00 +1100

    fix path json

--- assets/js/cityHelper.js
+++ assets/js/cityHelper.js
@@ -3,7 +3,7 @@ const ul_cityList = document.getElementById("ul_sityList");
 const inputDriveTo = document.getElementById("driveTo");
 const inputPhoneNumber = document.getElementById("phoneNumber");
 const lstCity = [];
-const resourceApi = fetch(`/assets/jsons/city.json`).then(r => r.json()).then(data => {
+const resourceApi = fetch(`/mishaSite/assets/jsons/city.json`).then(r => r.json()).then(data => {
     lstCity.push(...data);
     const pm_from = new PopupMenu(inputDriveFrom, lstCity);
     new PopupMenu(inputDriveTo, lstCity);
